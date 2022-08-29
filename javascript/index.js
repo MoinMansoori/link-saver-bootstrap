65c2a9d95591859a388dba044e33ad919479abe7
moinmansoori <moinmansuri1998@gmail.com>
2022-08-30 01:23:02 +0530

    save Tab not working

--- javascript/index.js
+++ javascript/index.js
@@ -5,36 +5,50 @@ const deleteButton = document.getElementById("deleteBtn")
 let inputBar = document.getElementById("floatingTextarea")
 let para = document.getElementById("paraElement")
 let links = document.getElementById("linkList")
-const saveTab = document.getElementById("saveTab")
+const saveTab1 = document.getElementById("saveTab")
+// const oldLinks = document.getElementById("oldLinks")
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem("currLeads"))
-if(leadsFromLocalStorage){
+if (leadsFromLocalStorage) {
     render(leadsFromLocalStorage)
 }
+// oldLinks.addEventListener("click", function(){
+//     let oldLinksfromLS = JSON.parse(localStorage.getItem("allLeads"))
+//     if(oldLinksfromLS){
+//         render(oldLinksfromLS)
+//     }
 
+// })
 
-deleteButton.addEventListener("dblclick", function(){
-    myLeads =[]
+function render(arrays) {
+    let renArray = ""
+    for (let i = 0; i < arrays.length; i++) {
+        renArray += `
+            <a href=${arrays[i]} class="list-group-item list-group-item-action" target = "_blank">${arrays[i]}</a>
+            `
+    }
+    links.innerHTML = renArray
+}
+deleteButton.addEventListener("dblclick", function () {
+    myLeads = []
     render(myLeads)
     localStorage.removeItem("currLeads")
 })
 
-function render(arrays){
-    let renArray = ""
-    for(let i=0; i < arrays.length; i++){
-        renArray += `
-        <a href=${arrays[i]} class="list-group-item list-group-item-action" target = "_blank">${arrays[i]}</a>
-        `
-    }
-    links.innerHTML = renArray
-}
-saveButton.addEventListener("click", function(){
+saveButton.addEventListener("click", function () {
     //console.log("Save Button Clicked")
     myLeads.push(inputBar.value)
     allLeads.push(inputBar.value)
     inputBar.value = ""
-    localStorage.setItem("allLeads", JSON.stringify(allLeads))
     localStorage.setItem("currLeads", JSON.stringify(myLeads))
+    // localStorage.setItem("allLeads", JSON.stringify(allLeads))
     render(myLeads)
 })
 
+saveTab1.addEventListener("click", function (){
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("currLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
 
--- javascript/oldLinks.js
+++ javascript/oldLinks.js
@@ -1,24 +1,24 @@
-const showBtn = document.getElementById("showOld")
-let links = document.getElementById("linkList")
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("allLeads"))
+// const showBtn = document.getElementById("showOld")
+// let links = document.getElementById("linkList")
+// let leadsFromLocalStorage = JSON.parse(localStorage.getItem("allLeads"))
 
-showBtn.addEventListener("click", function(){
+// showBtn.addEventListener("click", function(){
 
-    if(leadsFromLocalStorage){
-        render(leadsFromLocalStorage)
-    }
-    let oldLeads = JSON.parse(localStorage.getItem("allLeads"))
-    if(oldLeads){
-        render(oldLeads)
-    }
+//     if(leadsFromLocalStorage){
+//         render(leadsFromLocalStorage)
+//     }
+//     let oldLeads = JSON.parse(localStorage.getItem("allLeads"))
+//     if(oldLeads){
+//         render(oldLeads)
+//     }
     
-    function render(arrays){
-        let renArray = ""
-        for(let i=0; i < arrays.length; i++){
-            renArray += `
-            <a href=${arrays[i]} class="list-group-item list-group-item-action" target = "_blank">${arrays[i]}</a>
-            `
-        }
-        links.innerHTML = renArray
-    }
-})
+//     function render(arrays){
+//         let renArray = ""
+//         for(let i=0; i < arrays.length; i++){
+//             renArray += `
+//             <a href=${arrays[i]} class="list-group-item list-group-item-action" target = "_blank">${arrays[i]}</a>
+//             `
+//         }
+//         links.innerHTML = renArray
+//     }
+// })
